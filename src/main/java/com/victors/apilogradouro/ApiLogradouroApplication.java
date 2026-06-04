c6dbda8cb979dd9fe06f64bb120bfac76ed53d50
Victor-Suander <victors.empresa@gmail.com>
2026-06-04 13:22:46 -0300

    chore: configuracao inicial do projeto

---
+++ src/main/java/com/victors/apilogradouro/ApiLogradouroApplication.java
@@ -0,0 +1,13 @@
+package com.victors.apilogradouro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLogradouroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLogradouroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victors/apilogradouro/ApiLogradouroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victors.apilogradouro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLogradouroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
